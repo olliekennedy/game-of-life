c0e98aca541a7a344e53a7169c83d8cb5c00cc62
olliekennedy <ollie.kennedy1@gmail.com>
2021-01-02 22:42:59 +0000

    it's a start

---
+++ script.js
@@ -0,0 +1,11 @@
+createCanvas(400, 400);
+
+
+background(0);
+
+function drawGrid() { //draw the contents of the grid onto a canvas
+  var c = document.getElementById(“myCanvas”);
+  var ctx = c.getContext(“2d”);
+}
+
+drawGrid()
